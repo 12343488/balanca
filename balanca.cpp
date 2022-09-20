72fd7dcfdec10d19409eef65962ff69cc89af685
Veninha <69736492+12343488@users.noreply.github.com>
2022-09-20 17:03:58 -0400

    inicio

--- balanca.cpp
+++ balanca.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <time.h>
-
+#include "regulated-input.h"
 
 class Reagente
 {
@@ -32,9 +32,34 @@ public:
 int main(int argc, char** argv) 
 {
 	Reagente a;
+	std::string comando;
 	
 	std::cout << "insira o nome:\n> ";
 	std::getline(std::cin, a.Nome);
+	
+	std::cout << "\ninsira o peso total(frasco e reagente):\n> ";
+	std::geline(std::cin, comando);
+	ValidadeInput(comando, a.PesoTotal);
+	
+	std::cout << "\ninsira o peso do container(não necessario caso não queira coloque 0):\n> ";
+	std::geline(std::cin, comando);
+	ValidadeInput(comando, a.PesoContainer);
+	
+	std::cout << "\ninsira a data de validade:\nDia: ";
+	std::geline(std::cin, comando);
+	ValidadeInput(comando, a.Validade->tm_mday);
+	
+	std::cout << "\n\nMês: ";
+	std::geline(std::cin, comando);
+	ValidadeInput(comando, a.Validade->tm_mon);
+	
+	std::cout << "\n\nAno: ";
+	std::geline(std::cin, comando);
+	ValidadeInput(comando, a.Validade->tm_year);
+	
+	std::cout << "\ninsira o tipo de alerta:\n> ";
+	std::geline(std::cin, comando);
+	ValidadeInput(comando, a.Alerta);
 	
 	a.Print();	
 }
---
+++ regulated-input.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+bool ValidadeInput(std::string input, float& result);
